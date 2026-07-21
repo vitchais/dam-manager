429ce341f661185c8ba9c468c52f3d04d174917b
vitchais <vitchaismoraco@gmail.com>
2026-07-21 03:25:22 -0300

    Adicionando relacionamento com clientes

---
+++ js/admin.js
@@ -0,0 +1,45 @@
+async function carregarReservas() {
+
+    const { data, error } = await supabaseClient
+        .from("reservas")
+        .select(`
+            *,
+            clientes (
+                nome,
+                telefone
+            )
+        `);
+
+    if (error) {
+        console.error(error);
+        alert("Erro ao carregar reservas.");
+        return;
+    }
+
+    console.log(data);
+
+    const tabela = document.getElementById("tabelaReservas");
+    tabela.innerHTML = "";
+
+    data.forEach(reserva => {
+
+        tabela.innerHTML += `
+            <tr>
+                <td>${reserva.clientes?.nome ?? "-"}</td>
+                <td>${reserva.clientes?.telefone ?? "-"}</td>
+                <td>${reserva.data_reserva ?? "-"}</td>
+                <td>${reserva.horario ?? "-"}</td>
+                <td>${reserva.quantidade_pessoas ?? "-"}</td>
+                <td>${reserva.ambiente ?? "-"}</td>
+                <td>${reserva.status ?? "-"}</td>
+                <td>
+                    <button>Ver</button>
+                </td>
+            </tr>
+        `;
+
+    });
+
+}
+
+carregarReservas();
